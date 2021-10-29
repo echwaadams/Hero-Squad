bff7fa3e35966e16e0ff31acfecf157d7cbaf983
echwaadams <adamsechwa18@gmail.com>
2021-10-29 20:33:50 +0300

    created main route to show all heroes

--- src/test/java/models/App.java
+++ src/test/java/models/App.java
@@ -11,5 +11,12 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+
+        // get: show all heroes
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Hero> heroes = Hero.getAll();
+            model.put("heroes", heroes);
+        }, new HandlebarsTemplateEngine());
     }
 }
