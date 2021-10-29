288324f98e65ddb7bae3c8dbc250fe0cee2d127f
echwaadams <adamsechwa18@gmail.com>
2021-10-29 19:52:28 +0300

    created get method.

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,5 +1,12 @@
 package models;
-
 public class Hero {
+    private String name;
 
+    public Hero(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,8 +11,19 @@ class HeroTest {
     @BeforeEach
     void setUp() {
     }
+    @Test
+    public void checkForCorrectInstantiationOfHeroes_true() throws Exception {
+        Hero hero = new Hero("Marvel");
+        assertEquals(true,hero instanceof Hero);
+    }
+    @Test
+    public void HeroInstantiatesWithName_true() throws Exception{
+        Hero hero = new Hero("Marvel");
+        assertEquals("Marvel", hero.getName());
+    }
 
     @AfterEach
     void tearDown() {
     }
+
 }
