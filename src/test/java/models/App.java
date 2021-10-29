7272016e17f26ac2611d6cd4957f09b5389a9602
echwaadams <adamsechwa18@gmail.com>
2021-10-29 20:27:37 +0300

    created App.java file and imported handlebars

---
+++ src/test/java/models/App.java
@@ -0,0 +1,15 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+    }
+}
