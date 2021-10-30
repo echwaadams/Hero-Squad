a688eb336b4daa0853a74b77a99c01bf1741bf10
echwaadams <adamsechwa18@gmail.com>
2021-10-30 22:32:38 +0300

    created route to show heroes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,24 @@
 import spark.ModelAndView;
 import static spark.Spark.*;
 import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        // get: show hero
+        get("/hero", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Hero> hero = Hero.getAllInstances();
+            model.put("hero",hero);
+            return new ModelAndView(model,"hero.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
